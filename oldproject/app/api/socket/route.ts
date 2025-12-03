cf2fc26b1e8671f36390848f3dfbff8bc753e448
Mamypopofxxm <97841467+Mamypopo@users.noreply.github.com>
2025-12-03 10:55:17 +0700

    up

--- oldproject/app/api/socket/route.ts
+++
@@ -1,6 +0,0 @@
-// This route is for Next.js to handle Socket.io
-// The actual Socket.io server will be initialized in a custom server file
-export async function GET() {
-  return new Response('Socket.io endpoint', { status: 200 })
-}
-
